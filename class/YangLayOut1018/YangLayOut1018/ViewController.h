04acc35f5465f4d3b863ed2470a52c3093dfe64e
yangdaesung <yanggogo2333@gmail.com>
2016-10-19 09:38:16 +0900

    1018class

---
+++ class/YangLayOut1018/YangLayOut1018/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YangLayOut1018
+//
+//  Created by Yang on 2016. 10. 18..
+//  Copyright © 2016년 Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ class/YangLayOut1018/YangLayOut1018/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  YangLayOut1018
+//
+//  Created by Yang on 2016. 10. 18..
+//  Copyright © 2016년 Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //상단에 스테이터스바를 앱크기에 맞춰서 오토레이아웃처럼 해주는 것 !
+    //[[UIApplication sharedApplication] statusBarFrame];
+    
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
